4ed6f728c677a1f34415ea684a20dd1b93d29806
David Bolivar <90874442+db167@users.noreply.github.com>
2023-02-09 14:44:28 +0000

    Initial commit

---
+++ food.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+
+
+class Food:
+  
+  def _init__(self, position, color, shape):
+    self.position = position
+    self.color = color
+    self.shape = shape
+    
+  
